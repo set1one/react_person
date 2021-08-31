a24b368528f039f9570f02ca189f9d3c6dac1295
Eugene Gnedenko <deadup4@gmail.com>
2021-08-31 13:47:52 +0300

    Updates

---
+++ precache-manifest.94d51e00c08c25261e42be7c4d9cf769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2a5c5fd4fef05ccba7b8cb621dde72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5838eebcc0a1c204e37b",
+    "url": "./static/css/main.2025ce52.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5838eebcc0a1c204e37b",
+    "url": "./static/js/main.5a7a358a.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
